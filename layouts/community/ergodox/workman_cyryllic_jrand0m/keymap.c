a46b2c1a76ce2ec4f36653d9d623d06b68aa7257
Mike Stetsyshyn <jays.demons@gmail.com>
2017-12-26 21:33:51 +0200

    cleaned up unnesesary code

--- layouts/community/ergodox/workman_cyryllic_jrand0m/keymap.c
+++ layouts/community/ergodox/workman_cyryllic_jrand0m/keymap.c
@@ -16,10 +16,6 @@ enum {
   TD_LSQBR_PLUS  = 1,
   TD_RSQBR_EQ    = 2,
 };
-enum my_keycodes {
-	JR_SW_OS = SAFE_RANGE,
-	
-};
 
 // TODO: Define layer names that make sense for the ErgoDox EZ.
 #define BASE 0 // default layer
@@ -45,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |  INS | PRTS |       |      |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 |      |      | Home |       | PgUp |        |      |\
  *                                 |Space |Delete|------|       |------| Enter  |Space |
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
@@ -67,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(DEAD),            KC_J,   KC_F,   KC_U,   KC_P,   KC_SCLN,		KC_BSLS,
                                   KC_Y,   KC_N,   KC_E,   KC_O,   KC_I,             	KC_QUOT,
              MEH_T(KC_NO),        KC_K,   KC_L,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   	KC_RSFT,
-                                          KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          	KC_FN4,
+                                          KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          	KC_NO,
              TO(CRYL),        KC_NO,
              KC_PGUP,
              KC_PGDN,KC_ENT,KC_SPC
@@ -155,52 +151,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_WBAK
 ),
-/* Keymap 4: Dead Symbol Layer
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |  %   |   &  |   ?  |   +  |   @  |      |           |      |   $  |   _  |   [  |   ]  |   !  |   F12  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   /  |   (  |   =  |   0  |   {  |------|           |------|   }  |   1  |   *  |   )  |   -  |   "    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   6  |   7  |   8  |   9  |   |  |      |           |      |   \  |   2  |   3  |   4  |   5  |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-// SYMBOLS
-[DEAD] = KEYMAP(
-       // left hand
-       KC_TRNS,   KC_F1,    KC_F2,    KC_F3,      KC_F4,    KC_F5,          KC_TRNS,
-       KC_TRNS,   M(100),   M(101),   M(102),     M(103),   M(104),         KC_TRNS,
-       KC_TRNS,   M(109),  M(110),  M(111),       M(112),   M(113),
-       KC_TRNS,   M(120),     M(121),     M(122),           M(123),     M(124),        KC_TRNS,
-       KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
-       // right hand
-       KC_TRNS, KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-       KC_TRNS, M(105),   M(106),   M(107),   M(108),   M(130),   KC_F12,
-                M(114),   M(115),   M(116),   M(117),   M(118),   M(119),
-       KC_TRNS, M(125),   M(126),   M(127),   M(128),   M(129),   KC_TRNS,
-                          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
-),
 
-/* Keymap 4: Cirillyc layer
+/* Keymap 3: Cirillyc layer
  * So since i want these letters to be agnostic to OS and keylayout of ED i decided to send just codes via macro
- * imitating native insertion of Unicode symbols via means of os.  
+ * imitating native insertion of Unicode symbols via means of os.
  * For Win it will be "alt + <code>" for unix
  * For Mac it will be "alt + <code>" for unix
  * For Lin it will be "alt + <code>" for unix
@@ -229,19 +183,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GESC,        KC_1,         KC_2,         KC_3,         KC_4,         KC_5,         KC_NO,
         KC_TAB,         UC(0x0439),   UC(0x0446),   UC(0x0443),   UC(0x043A),   UC(0x0435),   KC_NO,
-        KC_BSPC,	 UC(0x0444),   UC(0x0438),   UC(0x0432),   UC(0x0430),   UC(0x043F),
+        KC_BSPC,	      UC(0x0444),   UC(0x0438),   UC(0x0432),   UC(0x0430),   UC(0x043F),
         KC_LSPO,        UC(0x044F),   UC(0x0447),   UC(0x0441),   UC(0x043C),   UC(0x0456),   KC_NO,
         KC_LCTL,        KC_NO,        KC_NO,        KC_LALT,      KC_LGUI,
                                                                     KC_INS,  KC_PSCR,
                                                                                     KC_HOME,
                                                                     KC_SPC, KC_DEL,KC_END,
         // right hand
-             KC_NO, KC_6,         KC_7,         KC_8,         KC_9,         KC_0,               KC_MINS,
-             KC_NO,           UC(0x043D),   UC(0x0433),   UC(0x0448),   UC(0x0449),   UC(0x0437),         UC(0x0445),
-                              UC(0x0440),   UC(0x043E),   UC(0x043B),   UC(0x0434),   UC(0x0436),         UC(0x0454),
-             KC_NO,           UC(0x0442),   UC(0x044C),   UC(0x0431),   UC(0x044E),   CTL_T(KC_SLSH),     KC_RSPC,
-                                      KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,            KC_FN4,
-             TO(BASE),        JR_SW_OS,
+             KC_NO,               KC_6,         KC_7,         KC_8,         KC_9,         KC_0,               KC_MINS,
+             KC_NO,               UC(0x043D),   UC(0x0433),   UC(0x0448),   UC(0x0449),   UC(0x0437),         UC(0x0445),
+                                  UC(0x0440),   UC(0x043E),   UC(0x043B),   UC(0x0434),   UC(0x0436),         UC(0x0454),
+             KC_NO,               UC(0x0442),   UC(0x044C),   UC(0x0431),   UC(0x044E),   CTL_T(KC_SLSH),     KC_RSPC,
+                                      KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,            KC_NO,
+             TO(BASE),        KC_NO,
              KC_PGUP,
              KC_PGDN,KC_ENT,KC_SPC
     ),
@@ -251,170 +205,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),                // FN1 - Momentary Layer 1 (Symbols)
     [2] = ACTION_LAYER_ON(BASE,0),                       // FN2 - Go back to the base layer
-    [3] = ACTION_LAYER_ON(DEAD,0),
-    [4] = ACTION_LAYER_TOGGLE(DEAD),
+    [3] = ACTION_LAYER_ON(SYMB,0),
 };
-
-//Tap Dance Definitions
-//qk_tap_dance_action_t tap_dance_actions[] = {
-  //Tap once for ';' , twice for ':'
-//  [TD_SCOL_COLN]  = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_COLN),
-//  [TD_LSQBR_PLUS] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_PLUS),
-//  [TD_RSQBR_EQ]   = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_EQL),
-// Other declarations would go here, separated by commas, if you have them
-//};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-          break;
-        case 100:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(5), U(LSFT), T(FN4), END);
-          break;
-        case 101:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(7), U(LSFT), T(FN4), END);
-          break;
-        case 102:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(SLSH), U(LSFT), T(FN4), END);
-          break;
-        case 103:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(EQL), U(LSFT), T(FN4), END);
-          break;
-        case 104:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(2), U(LSFT), T(FN4), END);
-          break;
-        case 105:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(4), U(LSFT), T(FN4), END);
-          break;
-        case 106:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(MINS), U(LSFT), T(FN4), END);
-          break;
-        case 107:
-          layer_clear();
-          return MACRODOWN(T(LBRC), T(FN4), END);
-          break;
-        case 108:
-          layer_clear();
-          return MACRODOWN(T(RBRC), T(FN4), END);
-          break;
-        case 130:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(1), U(LSFT), T(FN4), END);
-          break;
-        case 109:
-          layer_clear();
-          return MACRODOWN(T(SLSH), T(FN4), END);
-          break;
-        case 110:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(9), U(LSFT), T(FN4), END);
-          break;
-        case 111:
-          layer_clear();
-          return MACRODOWN(T(EQL), T(FN4), END);
-          break;
-        case 112:
-          layer_clear();
-          return MACRODOWN(T(0), T(FN4), END);
-          break;
-        case 113:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(LBRC), U(LSFT), T(FN4), END);
-          break;
-        case 114:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(RBRC), U(LSFT), T(FN4), END);
-          break;
-        case 115:
-          layer_clear();
-          return MACRODOWN(T(1), T(FN4), END);
-          break;
-        case 116:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(8), U(LSFT), T(FN4), END);
-          break;
-        case 117:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(0), U(LSFT), T(FN4), END);
-          break;
-        case 118:
-          layer_clear();
-          return MACRODOWN(T(MINS), T(FN4), END);
-          break;
-        case 119:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(QUOT), U(LSFT), T(FN4), END);
-          break;
-        case 120:
-          layer_clear();
-          return MACRODOWN(T(6), T(FN4), END);
-          break;
-        case 121:
-          layer_clear();
-          return MACRODOWN(T(7), T(FN4), END);
-          break;
-        case 122:
-          layer_clear();
-          return MACRODOWN(T(8), T(FN4), END);
-          break;
-        case 123:
-          layer_clear();
-          return MACRODOWN(T(9), T(FN4), END);
-          break;
-        case 124:
-          layer_clear();
-          return MACRODOWN(D(LSFT), T(BSLS), U(LSFT), T(FN4), END);
-          break;
-        case 125:
-          layer_clear();
-          return MACRODOWN(T(BSLS), T(FN4), END);
-          break;
-        case 126:
-          layer_clear();
-          return MACRODOWN(T(2), T(FN4), END);
-          break;
-        case 127:
-          layer_clear();
-          return MACRODOWN(T(3), T(FN4), END);
-          break;
-        case 128:
-          layer_clear();
-          return MACRODOWN(T(4), T(FN4), END);
-          break;
-        case 129:
-          layer_clear();
-          return MACRODOWN(T(5), T(FN4), END);
-          break;
-
-        default:
-        return MACRO_NONE;
-      }
-  return MACRO_NONE;
-};
-
-void set_unicode_wrp(uint8_t target) {
-	set_unicode_input_mode(target);
-}
-
-// Runs just one time when the keyboard initializes.
-void matrix_init_user(void) {
-    set_unicode_wrp(UC_WINC);
-};
-
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
@@ -437,22 +229,4 @@ void matrix_scan_user(void) {
             // none
             break;
     }
-};
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	//uint8_t currentVal;
-  switch (keycode) {
-    case JR_SW_OS:
-		/*currentVal = get_unicode_input_mode();
-		if (currentVal < UC_WINC){
-			currentVal = currentVal + 1;
-			set_unicode_input_mode(currentVal);
-		} else {
-			set_unicode_input_mode(UC_OSX);
-		}*/
-	return true;
-      break;
-	default:
-	return true;
-	break;
-  }
 };
--- layouts/community/ergodox/workman_cyryllic_jrand0m/visualizer.c
+++ layouts/community/ergodox/workman_cyryllic_jrand0m/visualizer.c
@@ -1,32 +1,7 @@
-/*
-Copyright 2017 Fred Sundvik
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "simple_visualizer.h"
 #include "process_unicode_common.h"
 #include <string.h>
 
-
-// This function should be implemented by the keymap visualizer
-// Don't change anything else than state->target_lcd_color and state->layer_text as that's the only thing
-// that the simple_visualizer assumes that you are updating
-// Also make sure that the buffer passed to state->layer_text remains valid until the previous animation is
-// stopped. This can be done by either double buffering it or by using constant strings
-static const char *oses[] = {"OS X","Linux","WinNative","BSD","WinCompose"};
-// i need to read from eeprom the value of the unicode set. eeprom_read_byte(EECONFIG_UNICODEMODE)
 static void get_visualizer_layer_and_color(visualizer_state_t* state) {
     uint8_t saturation = 60;
     if (state->status.leds & (1u << USB_LED_CAPS_LOCK)) {
@@ -34,11 +9,8 @@ static void get_visualizer_layer_and_color(visualizer_state_t* state) {
     }
     if (state->status.layer & 0x10) {
         state->target_lcd_color = LCD_COLOR(216, saturation, 0xFF);
-		uint8_t unicodeB = get_unicode_input_mode();
-		char *result = "Ukrainian - ";//12
-		strcat(result,oses[unicodeB]);
-        state->layer_text = "go away from here";//result;
-		
+        state->layer_text = "go away from here";
+
     }
     else if (state->status.layer & 0x8) {
         state->target_lcd_color = LCD_COLOR(100, saturation, 0xFF);
